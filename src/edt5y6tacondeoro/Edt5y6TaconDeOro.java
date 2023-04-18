769ce1864f551a9bb4ecdc88003bb809c322ab51
Izan30 <izangallegotorres@gmail.com>
2023-04-18 13:36:18 +0200

    prueba 2

--- src/edt5y6tacondeoro/Edt5y6TaconDeOro.java
+++ src/edt5y6tacondeoro/Edt5y6TaconDeOro.java
@@ -16,8 +16,8 @@ public class Edt5y6TaconDeOro {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        SistemaVP vp= new SistemaVP();//vs= ventana principal del sistema
-        vp.setVisible(true);
+        SistemaVP vs= new SistemaVP();//vs= ventana principal del sistema
+        vs.setVisible(true);
     }
     
 }
